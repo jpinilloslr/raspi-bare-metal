0436f634252d6c466e8ef08c0c6809f86527e620
Joaquin Pinillos <jpinilloslr@gmail.com>
2023-07-15 21:02:04 +0200

    Add check_lsr_reg

--- src/kernel.c
+++ src/kernel.c
@@ -4,6 +4,7 @@
 
 static void _print_menu();
 static void _check_el();
+static void _check_lsr_reg();
 
 void main()
 {
@@ -18,12 +19,15 @@ void main()
         char o = uart_read();
         if (o == '1')
             _check_el();
+        if (o == '2')
+            _check_lsr_reg();
     }
 }
 
 static void _print_menu()
 {
     printf("1:    Check current exception level\r\n");
+    printf("2:    Check AUX_MU_LSR_REG\r\n");
     printf("\r\n");
 }
 
@@ -31,4 +35,30 @@ static void _check_el()
 {
     printf("Current exception level: %d\r\n", get_el());
     printf("\r\n");
+}
+
+static void _check_lsr_reg()
+{
+    unsigned int last_value = 1234;
+    while (1)
+    {
+        unsigned int reg = reg_get32(AUX_MU_LSR_REG);
+        if (last_value != reg)
+        {
+            printf("AUX_MU_LSR_REG changed: %d\r\n", reg);
+            last_value = reg;
+        }
+
+        if (reg == AUX_MU_LSR_DATA_READY)
+        {
+            char c = uart_read();
+            printf("Data ready, reading: %d\r\n", c);
+
+            if (c == 'q')
+            {
+                printf("Exiting loop\r\n");
+                break;
+            }
+        }
+    }
 }
